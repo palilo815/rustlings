9ef97c6956639c7b530bece4b68ce98ad71d4e7f
palilo815 <hyunsoo1397@gmail.com>
2025-01-02 23:03:22 +0900

    done

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,9 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(apple: i32) -> i32 {
+    apple * if apple > 40 { 1 } else { 2 }
+}
 
 fn main() {
     // You can optionally experiment here.
