ab425ea46c717db77113652994a8de375e3dd446
palilo815 <hyunsoo1397@gmail.com>
2025-01-03 23:21:09 +0900

    done

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,16 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    String::from(input) + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
@@ -33,13 +36,7 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(
-            replace_me("I think cars are cool"),
-            "I think balloons are cool",
-        );
-        assert_eq!(
-            replace_me("I love to look at cars"),
-            "I love to look at balloons",
-        );
+        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool",);
+        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons",);
     }
 }
