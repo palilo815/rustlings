2c8641d829e00a65ee37b7a3813bc22450ed8f25
palilo815 <hyunsoo1397@gmail.com>
2025-01-04 22:00:32 +0900

    done

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,16 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(s, cmd)| match cmd {
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_string(),
+                Command::Append(cnt) => s + &"bar".repeat(cnt),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -38,6 +48,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
@@ -52,12 +63,7 @@ mod tests {
 
         assert_eq!(
             output,
-            [
-                "HELLO",
-                "all roads lead to rome!",
-                "foobar",
-                "barbarbarbarbarbar",
-            ]
+            ["HELLO", "all roads lead to rome!", "foobar", "barbarbarbarbarbar",]
         );
     }
 }
